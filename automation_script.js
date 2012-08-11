3bb7653fa660fbbaa43db3881ed96ef9e442a8aa
Jonathan Penn <jonathan@navel-labs.com>
2012-08-11 06:53:13 -0400

    Print a message on success, too

--- automation_script.js
+++ automation_script.js
@@ -10,6 +10,7 @@ var text = DetailScreen.displayedText();
 if (text != "coffee") {
   UIALogger.logError("Expected to see 'coffee'");
 } else {
+  UIALogger.logMessage("Saw 'coffee'!");
   DetailScreen.tapBackButton();
 }
 
